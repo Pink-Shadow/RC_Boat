9cda4f8ede99cd6d1c98fed22b7ebd713e219222
Jasper <74651087+Pink-Shadow@users.noreply.github.com>
2021-03-30 13:16:58 +0200

    Update RPI_CAM.py
    updated RPI_CAM

--- RPI_CAM.py
+++ RPI_CAM.py
@@ -16,7 +16,7 @@ PAGE="""\
 </head>
 <body>
 <center><h1>Raspberry Pi - RC_Boat Front View</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
+<center><img src="stream.mjpg" width="480" height="640"></center>
 </body>
 </html>
 """
