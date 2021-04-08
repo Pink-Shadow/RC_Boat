546c8f52daa5f13f3f18c5d7e15393115372738f
Wicked441 <74775601+Wicked441@users.noreply.github.com>
2021-04-08 12:25:53 +0200

    Update muziek.hpp

--- muziek.hpp
+++ muziek.hpp
@@ -1,8 +1,18 @@
 #ifndef MUZIEK_HPP
+
 #define MUZIEK_HPP
 
+
+
 #include <string>
+
 using namespace std;
+
 void play(string song);
 
+float afstand(int echo, int trig);
+
+
+
 #endif //PIPROGRAMMING_MUZIEK_H
+
